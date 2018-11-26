b610c98f31f2a88cc73e20cde70161f70cb476a7
fablerq <44864410+fablerq@users.noreply.github.com>
2018-11-26 23:15:36 +0300

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,7 @@ import App from './components/App.vue'
 import ExampleCrudUsers from './components/examples/ExampleCrudUsers.vue'
 import ExampleCrudOrders from './components/examples/ExampleCrudOrders.vue'
 import ExampleCrudProducts from './components/examples/ExampleCrudProducts.vue'
-import ExampleCrudFaculties from './components/examples/faculties.vue'
+import ExampleCrudFaculties from './components/examples/ExampleCrudFaculties.vue'
 
 const router = new VueRouter({
   routes: [
