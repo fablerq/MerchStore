66e779ec731be27b782b0e017c908cb385b7261b
fablerq <44864410+fablerq@users.noreply.github.com>
2018-11-27 22:19:54 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://merchstore1.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
