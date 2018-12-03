844e2ae339019e4d49cfc8a221dfb233bab724a0
fablerq <44864410+fablerq@users.noreply.github.com>
2018-12-03 04:31:17 +0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        //Schema::defaultStringLength(191);
     }
 
     /**
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-            $this->app['request']->server->set('HTTPS', true);
+         $this->app['request']->server->set('HTTPS', true);
     }
 }
