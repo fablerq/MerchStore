56ac1fef7f3c919ca7756894e52a188a8887e4e5
vladislav.bochmanov <vladislavbochmanov@gmail.com>
2018-12-07 20:05:15 +0300

    Новые компоненты

--- resources/js/components/vheader.vue
+++ resources/js/components/vheader.vue
@@ -1,45 +1,32 @@
 <template>
     <header>
-        <div class="container">
-            <div class="row justify-content-between align-items-center">
-                <div class="col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3">
-                    <router-link class="menu-link" :to="{ name: 'main' }"><img  class="mainlogo" src="http://localhost/MerchStore/resources/imgs/mainlogo.png" alt="logo"></router-link>
-                </div>
-                <div class="col-9 col-sm-9 col-md-9 col-lg-9 col-xl-9">
-                    <nav>
-                        <ul class="menu">
-                            <li>
-                                <div class="menu-item catalog">
-                                    <img src="http://localhost/MerchStore/resources/imgs/catalog.png" alt="catalog">
-                                    <router-link class="menu-link" :to="{ name: 'catalog' }">Каталог</router-link>
-                                </div>
-                            </li>
-                            <li class="slash"><img src="http://localhost/MerchStore/resources/imgs/slash.png" alt=""></li>
-                            <li>
-                                <div class="menu-item faq">
-                                    <img src="http://localhost/MerchStore/resources/imgs/faq.png" alt="catalog">
-                                    <router-link class="menu-link" :to="{ name: '' }">F&Q</router-link>
-                                </div>
-                            </li>
-                            <li class="slash"><img src="http://localhost/MerchStore/resources/imgs/slash.png" alt=""></li>
-                            <li>
-                                <div class="menu-item contacts">
-                                    <img src="http://localhost/MerchStore/resources/imgs/contacts.png" alt="catalog">
-                                    <router-link class="menu-link" :to="{ name: 'contacts' }">Контакты</router-link>
-                                </div>
-                            </li>
-                            <li class="slash"><img src="http://localhost/MerchStore/resources/imgs/slash.png" alt=""></li>
-                            <li>
-                                <div class="menu-item profile">
-                                    <img src="http://localhost/MerchStore/resources/imgs/profile.png" alt="catalog">
-                                    <router-link class="menu-link" :to="{ name: 'lkorders' }">Личный кабинет</router-link>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
+       
+                <b-navbar class="navigashka" toggleable="lg" type="light">
+                    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+                    <b-navbar-brand><b-link class="menu-link" :to="{ name: 'main' }"><img  class="mainlogo" src="http://localhost/MerchStore/resources/imgs/mainlogo.png" alt="logo"></b-link></b-navbar-brand>
+
+                    <b-collapse is-nav id="nav_collapse">
+                     
+                       
+
+                        <b-navbar-nav class="ml-auto">
+                             <b-navbar-nav>
+                            <b-nav-item><img style="width: 40px" src="http://localhost/MerchStore/resources/imgs/catalog.png" alt="catalog"><router-link class="menu-link" :to="{ name: 'catalog' }">Каталог</router-link></b-nav-item>
+                            <b-nav-item><img style="width: 40px" src="http://localhost/MerchStore/resources/imgs/faq.png" alt="catalog"><router-link class="menu-link" :to="{ name: 'faq' }">F&Q</router-link></b-nav-item>
+                            <b-nav-item><img style="width: 40px" src="http://localhost/MerchStore/resources/imgs/profile.png" alt="catalog"><router-link class="menu-link" :to="{ name: 'lkorders' }">Личный кабинет</router-link></b-nav-item>
+                            <b-nav-item><img style="width: 40px" src="http://localhost/MerchStore/resources/imgs/card.png" alt="card"><router-link class="menu-link" :to="{ name: 'card' }">Корзина</router-link></b-nav-item>
+                        </b-navbar-nav>
+
+
+     
+
+    </b-navbar-nav>
+
+
+                    </b-collapse>
+                </b-navbar>
+
     </header>
 </template>
 
@@ -50,41 +37,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-header {
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    padding: 1% 0;
-    margin-bottom: 1%;
 
-    .menu {
-        list-style: none;
-        display: flex;
-        height: 100%;
-        align-items: baseline;
-        justify-content: flex-end;
-
-        .menu-item {
-            display: flex;
-            flex-direction: column;
-            width: 180px;
-            height: auto;
-            align-items: center;
-        }
-
-        .slash {
-            align-self: center;
-        }
-        
-        li {
-            text-decoration: none;
-            margin-right: 15px;
-
-            .menu-link {
-                color: black;
-                font-size: 23px;
-            }
-        }
+    .navigashka {
+        background: #fff;
     }
-}
+
+    .mainlogo {
+        width: 120px;
+    }
+
+    .menu-link {
+        color: black;
+        margin-left: 15px;
+    }
+
+    b-nav-item img {
+        width: 30px;
+    }
 </style>
